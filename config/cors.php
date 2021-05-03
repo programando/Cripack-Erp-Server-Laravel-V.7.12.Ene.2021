7405fd0336729b8856fdd5c1bbada2c9388a7f3d
programando <jhonjamesmg@hotmail.com>
2021-05-03 14:33:01 -0500

    Cors

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths'                    => ['*','login','logout','reset/password','update/password','sanctum/csrf-cookie', 'user'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:3000','https://cripack.com', 'https://webcross.cripack.com:3000'],
+    'allowed_origins'          => ['http://localhost:3000','https://cripack.com', 'https://webcross.cripack.com/'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => false,
